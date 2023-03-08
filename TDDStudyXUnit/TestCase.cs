c7ffd1d6c746e08d594afda58fd89fdded67ca28
yajium <yajiuma720@yahoo.co.jp>
2023-03-08 23:50:08 +0900

    feat: add xunit project

---
+++ TDDStudyXUnit/TestCase.cs
@@ -0,0 +1,12 @@
+﻿namespace TDDStudyXUnit
+{
+    public class TestCase
+    {
+        protected string name;
+
+        public TestCase(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ TDDStudyXUnit/WasRun.cs
@@ -0,0 +1,22 @@
+﻿namespace TDDStudyXUnit
+{
+    public class WasRun : TestCase
+    {
+        public bool wasRun;
+
+        public WasRun(string name) : base(name)
+        {
+            wasRun = false;
+        }
+
+        public void Run()
+        {
+            TestMethod();
+        }
+
+        private void TestMethod()
+        {
+            wasRun = true;
+        }
+    }
+}
